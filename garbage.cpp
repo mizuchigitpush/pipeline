f3a5b140442c534b6cb5379fb980e2f791b477d9
Jacob Rippe <jrip03@gmail.com>
2020-02-01 00:22:59 -0500

    Update garbage.cpp

--- garbage.cpp
+++ garbage.cpp
@@ -14,10 +14,15 @@ int high_V;
 //rararar
 using namespace cv;
 using namespace std;
-
-void process(img) {
+   
+  Mat element = getStructuringElement(MORPH_ELIPSE, anchor=Point(10, 10));
+  
   cvtColor(img, hsvimg, CV_BGR2HSV); //be carefull, opencv "H" value max is 180
   inRange(hsvimg, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), thresh);
-  morphologyEx( src, dst, operation, element ) //fix later ahh
+  morphologyEx(thresh, close, 3, element); //fix later ahh
+  morphologyEx(close, open, 2, element);
   
-}
+  //contours, hierarchy = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+  findContours(opening, contours, CV_RETR_TREE, CHAIN_APPROX_NONE);
+  
+  
